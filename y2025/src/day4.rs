fbece454cf0ba246bc48c7339368ade7a8b7fd18
Brendon Fallquist <bfallquist@gmail.com>
2025-12-05 15:44:44 -0500

    day4 - part1

--- y2025/src/day4.rs
+++ y2025/src/day4.rs
@@ -1,7 +1,50 @@
-pub fn puzzle(_input: &str) -> usize {
-    todo!("puzzle");
+use ndarray::{Array2, array};
+use ndarray_conv::{ConvExt, ConvMode, PaddingMode};
+use tracing::info;
+
+pub fn puzzle(input: &str) -> usize {
+    if input.is_empty() {
+        return 0;
+    }
+
+    let input = transform_str_to_ndarray(input);
+    info!("{input:?}");
+
+    let kernel = array![[
+        1u8, 1u8, 1u8
+    ],[
+        1u8, 0u8, 1u8
+    ],[
+        1u8, 1u8, 1u8
+    ]];
+
+    let sums = input.conv(&kernel, ConvMode::Same, PaddingMode::Zeros).unwrap();
+    info!("{sums:?}");
+
+    let result = input.iter().zip(sums.iter()).filter(|(input, conv)| (input == &&1) && (conv < &&4)).count();
+
+    result
 }
 
+fn transform_str_to_ndarray(input: &str) -> Array2<u8> {
+    let raw_vec = input
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|char| match char {
+                    '@' => 1,
+                    _ => 0,
+                })
+                .collect::<Vec<u8>>()
+        })
+        .collect::<Vec<Vec<u8>>>();
+
+    Array2::from_shape_vec(
+        (raw_vec.len(), raw_vec[0].len()),
+        raw_vec.into_iter().flatten().collect(),
+    )
+    .unwrap()
+}
 
 #[cfg(test)]
 mod tests {
@@ -35,6 +78,6 @@ mod tests {
     #[test]
     fn test_input() {
         let result = puzzle(include_str!("day4_input.txt"));
-        assert_eq!(result, 0);
+        assert_eq!(result, 1435);
     }
 }
