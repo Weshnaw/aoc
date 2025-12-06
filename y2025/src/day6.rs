b6fbbca6cc168ae6732149e760d42f5834243aa8
Brendon Fallquist <bfallquist@gmail.com>
2025-12-06 01:08:18 -0500

    init day6

---
+++ y2025/src/day6.rs
@@ -0,0 +1,31 @@
+pub fn puzzle(_input: &str) -> (u64, u64) {
+    todo!("puzzle");
+}
+
+#[cfg(test)]
+mod tests {
+    use test_log::test;
+
+    use super::*;
+
+    #[test]
+    fn test_empty_input() {
+        let result = puzzle("");
+        assert_eq!(result, (0, 0));
+    }
+
+    #[test]
+    fn test_example_input() {
+        let result = puzzle("123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  ");
+        assert_eq!(result, (4277556, 0));
+    }
+
+    #[test]
+    fn test_input() {
+        let result = puzzle(include_str!("day0_input.txt"));
+        assert_eq!(result, (0, 0));
+    }
+}
