e9c4c9fec78d655d7cda1f8718deb359cd55d3d6
Brendon Fallquist <bfallquist@gmail.com>
2025-12-07 12:08:48 -0500

    day7 - completed

--- y2025/src/day7.rs
+++ y2025/src/day7.rs
@@ -1,18 +1,14 @@
 use bitvec::prelude::*;
-use tracing::debug;
+use tracing::{debug, info};
 
 pub fn puzzle(input: &str) -> (usize, u64) {
-    (part1(input), 0)
-}
-
-fn part1(input: &str) -> usize {
-    input
+    let (_, splits, paths) = input
         .lines()
-        .map(|l| (parse_line(l), 0))
+        .map(|l| (parse_line(l), 0, vec![]))
         .filter(|l| l.0.any())
         .reduce(split_tachyons)
-        .unwrap_or_default()
-        .1
+        .unwrap_or_default();
+    (splits, paths.iter().sum())
 }
 
 fn parse_line(line: &str) -> BitVec {
@@ -33,9 +29,9 @@ fn parse_line(line: &str) -> BitVec {
 }
 
 fn split_tachyons(
-    (current_beams, current_count): (BitVec, usize),
-    (tachyon_splitters, _): (BitVec, usize),
-) -> (BitVec, usize) {
+    (current_beams, current_count, particles): (BitVec, usize, Vec<u64>),
+    (tachyon_splitters, _, _): (BitVec, usize, Vec<u64>),
+) -> (BitVec, usize, Vec<u64>) {
     let splitter_hits = current_beams.clone() & &tachyon_splitters;
     let splitter_beams_removed = !splitter_hits.clone() & &current_beams;
     let mut split_left = splitter_hits.clone();
@@ -47,11 +43,28 @@ fn split_tachyons(
 
     let new_count = current_count + splitter_hits.count_ones();
 
+    let mut particles = if particles.is_empty() {
+        let mut particles = vec![0; current_beams.len()];
+        for beam in current_beams.iter_ones() {
+            particles[beam] = 1;
+        }
+        info!(?particles);
+        particles
+    } else {
+        particles
+    };
+
+    for split in splitter_hits.iter_ones() {
+        particles[split + 1] += particles[split];
+        particles[split - 1] += particles[split];
+        particles[split] = 0;
+    }
+
     debug!(
-        "{current_beams:015b} + {tachyon_splitters:015b} = {result:015b} ({current_count} => {new_count})"
+        "{current_beams:015b} + {tachyon_splitters:015b} = {result:015b} |{particles:02?}| ({current_count:02} => {new_count:02})"
     );
 
-    (result, new_count)
+    (result, new_count, particles)
 }
 
 #[cfg(test)]
@@ -72,17 +85,18 @@ mod tests {
         let result = parse_line(input);
         assert_eq!(result, expected);
     }
+
     #[test]
     #[rstest]
-    #[case(bitvec!(0,0,0,0,0,0,0,1,0,0,0,0,0,0,0), bitvec!(0,0,0,0,0,0,0,1,0,0,0,0,0,0,0), (bitvec!(0,0,0,0,0,0,1,0,1,0,0,0,0,0,0), 1))]
-    #[case(bitvec!(0,0,0,0,0,0,1,0,1,0,0,0,0,0,0), bitvec!(0,0,0,0,0,0,1,0,1,0,0,0,0,0,0), (bitvec!(0,0,0,0,0,1,0,1,0,1,0,0,0,0,0), 2))]
-    #[case(bitvec!(0,0,0,0,0,1,0,1,0,1,0,0,0,0,0), bitvec!(0,0,0,0,0,1,0,0,0,0,0,0,0,0,0), (bitvec!(0,0,0,0,1,0,1,1,0,1,0,0,0,0,0), 1))]
+    #[case(bitvec!(0,0,0,0,0,0,0,1,0,0,0,0,0,0,0), bitvec!(0,0,0,0,0,0,0,1,0,0,0,0,0,0,0), (bitvec!(0,0,0,0,0,0,1,0,1,0,0,0,0,0,0), 1, vec![0,0,0,0,0,0,1,0,1,0,0,0,0,0,0]))]
+    #[case(bitvec!(0,0,0,0,0,0,1,0,1,0,0,0,0,0,0), bitvec!(0,0,0,0,0,0,1,0,1,0,0,0,0,0,0), (bitvec!(0,0,0,0,0,1,0,1,0,1,0,0,0,0,0), 2, vec![0,0,0,0,0,1,0,2,0,1,0,0,0,0,0]))]
+    #[case(bitvec!(0,0,0,0,0,1,0,1,0,1,0,0,0,0,0), bitvec!(0,0,0,0,0,1,0,0,0,0,0,0,0,0,0), (bitvec!(0,0,0,0,1,0,1,1,0,1,0,0,0,0,0), 1, vec![0,0,0,0,1,0,1,1,0,1,0,0,0,0,0]))]
     fn test_split_tachyons(
         #[case] current: BitVec,
         #[case] splitters: BitVec,
-        #[case] expected: (BitVec, usize),
+        #[case] expected: (BitVec, usize, Vec<u64>),
     ) {
-        let result = split_tachyons((current, 0), (splitters, 0));
+        let result = split_tachyons((current, 0, vec![]), (splitters, 0, vec![]));
         assert_eq!(result, expected);
     }
 
@@ -113,12 +127,12 @@ mod tests {
 .^.^.^.^.^...^.
 ...............",
         );
-        assert_eq!(result, (21, 0));
+        assert_eq!(result, (21, 40));
     }
 
     #[test]
     fn test_input() {
         let result = puzzle(include_str!("day7_input.txt"));
-        assert_eq!(result, (1539, 0));
+        assert_eq!(result, (1539, 6479180385864));
     }
 }
