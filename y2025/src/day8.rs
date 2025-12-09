b415dfe0942ab11beb5fb45e902b3e6da874d0ac
Brendon Fallquist <bfallquist@gmail.com>
2025-12-09 15:06:47 -0500

    day8 - minor cleanup in tests

--- y2025/src/day8.rs
+++ y2025/src/day8.rs
@@ -146,10 +146,7 @@ mod tests {
 
     use super::*;
 
-    #[test]
-    fn test_puzzle_1_example_input() {
-        let result = puzzle_1(
-            "\
+    const EXAMPLE: &str = "\
 162,817,812
 57,618,57
 906,360,560
@@ -169,49 +166,31 @@ mod tests {
 941,993,340
 862,61,35
 984,92,344
-425,690,689",
-            10,
-        );
+425,690,689";
+
+    const INPUT: &str = include_str!("day8_input.txt");
+
+    #[test]
+    fn test_puzzle_1_example_input() {
+        let result = puzzle_1(EXAMPLE, 10);
         assert_eq!(result, 40);
     }
 
     #[test]
     fn test_puzzle_1_input() {
-        let result = puzzle_1(include_str!("day8_input.txt"), 1_000);
+        let result = puzzle_1(INPUT, 1_000);
         assert_eq!(result, 129564);
     }
 
     #[test]
     fn test_puzzle_2_example_input() {
-        let result = puzzle_2(
-            "\
-162,817,812
-57,618,57
-906,360,560
-592,479,940
-352,342,300
-466,668,158
-542,29,236
-431,825,988
-739,650,466
-52,470,668
-216,146,977
-819,987,18
-117,168,530
-805,96,715
-346,949,466
-970,615,88
-941,993,340
-862,61,35
-984,92,344
-425,690,689",
-        );
+        let result = puzzle_2(EXAMPLE);
         assert_eq!(result, 25272.);
     }
 
     #[test]
     fn test_puzzle_2_input() {
-        let result = puzzle_2(include_str!("day8_input.txt"));
+        let result = puzzle_2(INPUT);
         assert_eq!(result, 42047840.0);
     }
 }
