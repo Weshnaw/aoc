7dd444a66902079e655e633d78c994cdddba4a01
Brendon Fallquist <bfallquist@gmail.com>
2025-12-09 13:14:58 -0500

    day9 - init

--- y2025/src/day9.rs
+++ y2025/src/day9.rs
@@ -10,13 +10,21 @@ mod tests {
 
     #[test]
     fn test_example_input() {
-        let result = puzzle("");
+        let result = puzzle("\
+7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3");
         assert_eq!(result, (0, 0));
     }
 
     #[test]
     fn test_input() {
-        let result = puzzle(include_str!("day0_input.txt"));
+        let result = puzzle(include_str!("day9_input.txt"));
         assert_eq!(result, (0, 0));
     }
 }
