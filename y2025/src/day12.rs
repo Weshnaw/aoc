dd0bd1c68052253ca8c7afc12de5f7829bf64adf
Brendon Fallquist <bfallquist@gmail.com>
2025-12-13 15:48:34 -0500

    day12 - init with parsing

---
+++ y2025/src/day12.rs
@@ -0,0 +1,162 @@
+use glam::U64Vec2;
+use ndarray::Array2;
+use tracing::info;
+use winnow::Parser;
+
+use crate::day12::parsing::parse_input;
+
+pub fn puzzle(input: &str) -> u64 {
+    let (_, input) = parse_input.parse_peek(input).unwrap();
+
+    info!(?input);
+
+    todo!("puzzle");
+}
+
+#[derive(Debug)]
+struct Input {
+    presents: Vec<Shape>,
+    trees: Vec<Tree>,
+}
+
+#[derive(Debug)]
+struct Shape {
+    shape: Array2<bool>,
+}
+
+#[derive(Debug)]
+struct Tree {
+    dimensions: U64Vec2,
+    present_counts: Vec<u64>,
+}
+
+mod parsing {
+    use super::*;
+    use winnow::{
+        ascii::{digit1, multispace1, space1},
+        combinator::{alt, preceded, repeat, separated, seq},
+    };
+
+    fn parse_taken_spaces(input: &mut &str) -> winnow::Result<Vec<bool>> {
+        repeat(3, alt(("#", ".")).map(|char| char == "#")).parse_next(input)
+    }
+
+    fn parse_shape(input: &mut &str) -> winnow::Result<Shape> {
+        separated(3, parse_taken_spaces, multispace1)
+            .map(|taken_spaces: Vec<_>| {
+                let flat: Vec<_> = taken_spaces.into_iter().flatten().collect();
+                let shape = Array2::from_shape_vec((3, 3), flat).unwrap();
+
+                let s = Shape { shape };
+                s
+            })
+            .parse_next(input)
+    }
+
+    fn parse_present(input: &mut &str) -> winnow::Result<Shape> {
+        preceded(seq!(digit1, ":", multispace1), parse_shape).parse_next(input)
+    }
+
+    fn parse_presents(input: &mut &str) -> winnow::Result<Vec<Shape>> {
+        separated(0.., parse_present, multispace1).parse_next(input)
+    }
+
+    fn parse_digits(input: &mut &str) -> winnow::Result<u64> {
+        digit1.try_map(str::parse::<u64>).parse_next(input)
+    }
+
+    fn parse_dimensions(input: &mut &str) -> winnow::Result<U64Vec2> {
+        seq!(U64Vec2 {
+            x: parse_digits,
+            _: "x",
+            y: parse_digits
+        })
+        .parse_next(input)
+    }
+
+    fn parse_present_counts(input: &mut &str) -> winnow::Result<Vec<u64>> {
+        separated(0.., parse_digits, space1).parse_next(input)
+    }
+
+    fn parse_tree(input: &mut &str) -> winnow::Result<Tree> {
+        seq!(
+            Tree {
+                dimensions: parse_dimensions,
+                _: ": ",
+                present_counts: parse_present_counts
+            }
+        )
+        .parse_next(input)
+    }
+
+    fn parse_trees(input: &mut &str) -> winnow::Result<Vec<Tree>> {
+        separated(0.., parse_tree, multispace1).parse_next(input)
+    }
+
+    pub fn parse_input(input: &mut &str) -> winnow::Result<Input> {
+        seq!(
+            Input {
+                presents: parse_presents,
+                _: multispace1,
+                trees: parse_trees
+            }
+        )
+        .parse_next(input)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use test_log::test;
+
+    use super::*;
+
+    const EXAMPLE: &str = "\
+0:
+###
+##.
+##.
+
+1:
+###
+##.
+.##
+
+2:
+.##
+###
+##.
+
+3:
+##.
+###
+##.
+
+4:
+###
+#..
+###
+
+5:
+###
+.#.
+###
+
+4x4: 0 0 0 0 2 0
+12x5: 1 0 1 0 2 2
+12x5: 1 0 1 0 3 2";
+    const INPUT: &str = include_str!("day12_input.txt");
+
+    #[test]
+    fn test_example_input() {
+        let result = puzzle(EXAMPLE);
+        assert_eq!(result, 2);
+    }
+
+    #[test]
+    #[ignore]
+    fn test_input() {
+        let result = puzzle(INPUT);
+        assert_eq!(result, 0);
+    }
+}
